f8c348efb48bb858a2faf669d13b5cbca4086f2c
Stephen Mount <stemount@gmail.com>
2020-07-31 15:39:20 +0100

    force this to be a js project

---
+++ index.js
@@ -0,0 +1,9 @@
+// let's use something small to test
+// I'm making a bonus index.js so it inherits the idea this *IS* a JS project.
+
+import { isUpperCase } from "is-upper-case";
+
+console.log(isUpperCase("string"));
+console.log(isUpperCase("YOUR FACE"));
+
+console.log(isUpperCase("YA DA'")); // For those with a scottish accent.
